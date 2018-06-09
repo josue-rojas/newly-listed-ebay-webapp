829db7c12f24ccb76e30c8d6078cc534cbb5593e
josue rojas <josuerojas.rojas@gmail.com>
2018-06-08 20:32:36 -0400

    basic start page done

---
+++ index.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let parser = require('body-parser');
+let fs = require("fs");
+let request = require('request');
+let cheerio = require('cheerio');
+let read_settings = fs.readFileSync("settings.json");
+let settings = JSON.parse(read_settings);
+const app = express();
+
+app.use(express.static('public/'));
+app.use(parser.json());
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+  res.render('pages/index.ejs');
+});
+
+app.get('/run', (req, res)=>{
+
+});
+
+app.get('/settings', (req, res)=>{
+
+});
+
+app.get('/*', (req, res)=>{
+  res.redirect('/');
+});
+
+
+
+app.listen(process.env.PORT || 8080);
+console.log('listening on port '+(process.env.PORT || 8080));
